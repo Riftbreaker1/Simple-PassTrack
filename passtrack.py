b5f01f6d894eb23a64e543716a67f195b4631e39
Riftbreaker1 <hydrogen.man72@gmail.com>
2026-05-14 22:59:34 +0100

    Update passtrack.py

--- passtrack.py
+++ passtrack.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import plotly.express as px
 import requests
-import time
-import math
-from datetime import datetime, timezone
 
 apikey = ""
 norad = input("NORAD ID:")
@@ -12,15 +9,25 @@ if norad == "":
 
 print("https://api.n2yo.com/rest/v1/satellite/radiopasses/" + norad + "/52.429/-1.887/" + "0/2/11/&apiKey=" + apikey)
 
+fact = requests.get("https://uselessfacts.jsph.pl/api/v2/facts/random")
+data = fact.json()
+
+fact1 = data["text"]
+
+print("Did you know? ", fact1)
+
 response = requests.get("https://api.n2yo.com/rest/v1/satellite/radiopasses/" + norad + "/52.429/-1.887/" + "0/2/11/&apiKey=" + apikey)
 data = response.json()
 
 passes = data["passes"]
+info = data["info"]
 max_elevations = [p["maxEl"] for p in passes]
 start_utcs = [p["startUTC"] for p in passes]
+sat_name = info["satname"]
 
 start_datetimes = pd.to_datetime(start_utcs, unit='s')
 
 fig = px.scatter(x=start_datetimes, y=max_elevations,
-                  labels={"x": "Pass Time (UTC)", "y": "Max Elevation (°)"})
+                  labels={"x": "Pass Time (UTC)", "y": "Max Elevation of " + sat_name + " (°)"},
+                  template="plotly_dark")
 fig.show()
